1a7ce5e326f5632c9e3af9a7d81d68cabcc330ca
SukanthK31 <sukak2831@gmail.com>
2025-02-18 17:14:41 +0530

    secound commit

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -2,6 +2,9 @@ import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs';
 import { cookies } from 'next/headers';
 import { NextResponse } from 'next/server';
 
+// Force the route to render dynamically
+export const dynamic = 'force-dynamic';
+
 export async function GET(request: Request) {
   const requestUrl = new URL(request.url);
   const code = requestUrl.searchParams.get('code');
